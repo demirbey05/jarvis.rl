0f498f38151864cb147379f4b08de9109dbe4295
Your Name <you@example.com>
2025-03-01 23:24:13 +0300

    MC exploring starts is implemented

--- monte_carlo.py
+++ monte_carlo.py
@@ -18,9 +18,9 @@ class MonteCarloAgent():
 
     def generate_episode(self, state=None, action=None,episode_length=30):
         if state is None:
-            state = self.env.reset()
+            state,_ = self.env.reset()
         else:
-            state = self.env.reset(options={"start_state": state})
+            state,_ = self.env.reset(options={"start_state": state})
         
         if action is None:
             # Choose randomly among optimal actions
@@ -56,6 +56,34 @@ class MonteCarloAgent():
             for state in range(self.env.observation_space.n):
                 best_action = np.argmax(self.q[state])
                 self.policy[state, best_action] = 1.0
+    
+    def mc_exploring_starts(self):
+        state_action_pairs = [(state, action) for state in range(self.env.observation_space.n) for action in range(self.env.action_space.n)]
+        returns = {(state, action): 0 for state in range(self.env.observation_space.n) for action in range(self.env.action_space.n)}
+        numbers = {(state, action): 0 for state in range(self.env.observation_space.n) for action in range(self.env.action_space.n)}
+        for i in tqdm(range(self.num_episodes)):
+            state_action_index = self.num_episodes % len(state_action_pairs)
+            state, action = state_action_pairs[state_action_index]
+            episode = self.generate_episode(state, action)
+            total_return = 0
+            for t in range(len(episode) - 1, -1, -1):
+                state, action, reward = episode[t]
+                total_return = self.gamma * total_return + reward
+                state_index = self.env._location_to_index(state["agent"])
+                returns[(state_index, action)] += total_return
+                numbers[(state_index, action)] += 1
+                self.q[state_index, action] = returns[(state_index, action)] / numbers[(state_index, action)]
+            
+            self.policy = np.zeros((self.env.observation_space.n, self.env.action_space.n))
+            for state in range(self.env.observation_space.n):
+                best_action = np.argmax(self.q[state])
+                self.policy[state, best_action] = 1.0
+                
+            
+            
+
+            
+                    
             
         
 
@@ -65,6 +93,13 @@ if __name__ == "__main__":
     from grid import GridWorldEnv
     env = GridWorldEnv(size=5)
     agent = MonteCarloAgent(env, num_episodes=1)
+    print("MC Basic")
+    print("------------")
     agent.mc_basic()
+    print("Policy Grid:")
+    env.render_policy(agent.policy)
+    print("MC Exploring Starts")
+    print("------------")
+    agent.mc_exploring_starts()
     print("Policy Grid:")
     env.render_policy(agent.policy)
